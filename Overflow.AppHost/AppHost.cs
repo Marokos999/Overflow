a92dc316589b5a6dad3edb084d54c9a8ad1ba8e9
Marko Jovanovic <marokos987@outlook.com>
2025-12-28 23:42:22 -0500

    Add DB server configuration and dependencies

--- Overflow.AppHost/AppHost.cs
+++ Overflow.AppHost/AppHost.cs
@@ -5,12 +5,21 @@ var builder = DistributedApplication.CreateBuilder(args);
 var keycloak = builder.AddContainer("keycloak", "quay.io/keycloak/keycloak", "26.4")
     .WithEnvironment("KEYCLOAK_ADMIN", "admin")
     .WithEnvironment("KEYCLOAK_ADMIN_PASSWORD", "admin")
+    .WithEnvironment("KC_LOG_LEVEL", "INFO")
     .WithHttpEndpoint(port: 6001, targetPort: 8080, name: "http")
     .WithBindMount("keycloak-data", "/opt/keycloak/data")
     .WithArgs("start-dev");
 
+var postgres = builder.AddPostgres("postgres", port: 5432)
+    .WithDataVolume("postgres-data")
+    .WithPgAdmin();
+
+var questionDb = postgres.AddDatabase("question-db");
+
 var questionService = builder.AddProject<QuestionService>("question-svc")
     .WithReference(keycloak.GetEndpoint("http"))   
-    .WaitFor(keycloak);
+    .WithReference(questionDb)
+    .WaitFor(keycloak)
+    .WaitFor(postgres);
 
 builder.Build().Run();
