2ba7d4ca471310e2d493224960803b82371fb79b
Marko Jovanovic <marokos987@outlook.com>
2025-12-28 21:24:21 -0500

    Keycloak

--- Overflow.AppHost/AppHost.cs
+++ Overflow.AppHost/AppHost.cs
@@ -4,6 +4,5 @@ builder.AddContainer("keycloak", "quay.io/keycloak/keycloak", "26.4")
     .WithEnvironment("KEYCLOAK_ADMIN", "admin")
     .WithEnvironment("KEYCLOAK_ADMIN_PASSWORD", "admin")
     .WithHttpEndpoint(port: 6001, targetPort: 8080)
-    .WithArgs("start-dev");
-
+    .WithArgs("start-dev");//nastavicemo
 builder.Build().Run();
